bd9ff53d3ec2b3ed2968de4a57115be44d3cb353
Reda <reda.rackauskaite@gmail.com>
2023-04-05 16:12:27 +0700

    Recreate rock paper scissors game to more cleaner vestion

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const getComputerChoice = Math.floor(Math.random() * 3);
-if (getComputerChoice == 0) {
-  console.log("rock");
-} else if (getComputerChoice == 1) {
-  console.log("paper");
-} else {
-  console.log("scissors");
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissors"];
+  return choice[Math.floor(Math.random() * choice.length)];
 }
-const computerSelection = getComputerChoice;
 
+// function playerSelection = (getPlayerChoice{
+//   let playerSelection = playerSelection.toLowerCase();
+//   if (
+//     playerSelection === "rock" ||
+//     playerSelection === "paper" ||
+//     playerSelection === "scissors"
+//   ) {
+//     console.log(playerSelection);
+//   } else {
+//     console.log("Input not valid");
+//   }
+// };
 
-const getPlayerChoice = (playerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  if(playerSelection === "rock", playerSelection === "paper", playerSelection === "scissors" ) {
-    console.log(playerSelection);
-  } else {
-    console.log("Input not valid");
-  }
-}
-);
+// console.log(getComputerChoice, playerSelection);
 
-const playerSelection = getPlayerChoice.toLowerCase();
-console.log(getComputerChoice, getComputerChoice);
+// // const playRound = (playerSelection, computerSelection) => {
+// //   if (playerSelection === computerSelection) {
+// //     console.log("It's a tie!");
+// //   } else if (
+// //     (playerSelection === "rock" && computerSelection === "paper") ||
+// //     (playerSelection === "paper" && computerSelection === "scissors") ||
+// //     (playerSelection === "scissors" && computerSelection === "rock")
+// //   ) {
+// //     console.log(
+// //       "You lost!" + playerSelection + " lost for " + computerSelection + "."
+// //     );
+// //   } else {
+// //     console.log(
+// //       "You won!" + playerSelection + " lost for " + computerSelection + "."
+// //     );
+// //   }
+// // };
+// // console.log(playRound);
