5a0004ca365326509ecc9e028c0a95e10f37810d
Reda <reda.rackauskaite@gmail.com>
2023-04-05 16:19:55 +0700

    Add computer and player choices in the game

--- script.js
+++ script.js
@@ -3,36 +3,21 @@ function getComputerChoice() {
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// function playerSelection = (getPlayerChoice{
-//   let playerSelection = playerSelection.toLowerCase();
-//   if (
-//     playerSelection === "rock" ||
-//     playerSelection === "paper" ||
-//     playerSelection === "scissors"
-//   ) {
-//     console.log(playerSelection);
-//   } else {
-//     console.log("Input not valid");
-//   }
-// };
-
-// console.log(getComputerChoice, playerSelection);
-
-// // const playRound = (playerSelection, computerSelection) => {
-// //   if (playerSelection === computerSelection) {
-// //     console.log("It's a tie!");
-// //   } else if (
-// //     (playerSelection === "rock" && computerSelection === "paper") ||
-// //     (playerSelection === "paper" && computerSelection === "scissors") ||
-// //     (playerSelection === "scissors" && computerSelection === "rock")
-// //   ) {
-// //     console.log(
-// //       "You lost!" + playerSelection + " lost for " + computerSelection + "."
-// //     );
-// //   } else {
-// //     console.log(
-// //       "You won!" + playerSelection + " lost for " + computerSelection + "."
-// //     );
-// //   }
-// // };
-// // console.log(playRound);
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `It's a tie!`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `You won the round!`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `You won the round!`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `You won the round!`;
+  } else {
+    return `You lost this round!`;
+  }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
